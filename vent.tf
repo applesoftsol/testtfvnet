da02906ed01c850aa278bd03d86f28fbbd757138
applesoftsol <63569208+applesoftsol@users.noreply.github.com>
2022-03-31 21:36:26 +0530

    Update vent.tf

--- vent.tf
+++ vent.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_resource_group" "example" {
-  name     = "tf_rg1"
+  name     = "tf_rg2"
   location = "West Europe"
   tags = {
 	env="dev"
@@ -11,20 +11,11 @@ resource "azurerm_virtual_network" "example" {
   name                = "vnet1"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = ["11.0.0.0/16"]
+  dns_servers         = ["11.0.0.4", "11.0.0.5"]
 
   subnet {
     name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-  }
-
-  tags = {
-    environment = "dev"
+    address_prefix = "11.0.1.0/24"
   }
 }
