3a2a7bc47422bdacbbdc2340344a6361bf442f64
Sabitha <Sabitha@example.com>
2022-03-31 20:23:12 +0530

    added tf files

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "48c7cd04-2b44-4873-b5aa-25b49bd06b94"
+  client_id       = "3bdd3137-525d-412a-b875-c4362a6a0810"
+  client_secret   = "hHI7Q~FX_Jrnbu1QKyOguCjExUDjY.gHIkyst"
+  tenant_id       = "1b478c1a-de95-4cca-b620-81b2773ddff3"
+}
---
+++ vent.tf
@@ -0,0 +1,30 @@
+
+resource "azurerm_resource_group" "example" {
+  name     = "tf_rg1"
+  location = "West Europe"
+  tags = {
+	env="dev"
+	createdby="applesoftsol"
+}
+}
+resource "azurerm_virtual_network" "example" {
+  name                = "vnet1"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
