cc74c7ce6a6d96bc52eac9020398b23e021d2f28
applesoftsol <63569208+applesoftsol@users.noreply.github.com>
2022-03-31 21:36:40 +0530

    Update vent.tf

--- vent.tf
+++ vent.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 }
 resource "azurerm_virtual_network" "example" {
-  name                = "vnet1"
+  name                = "vnet2"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["11.0.0.0/16"]
